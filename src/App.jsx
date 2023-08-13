6bb0e821811a07cac781c42a26d045cbf42bcb50
yejb <yejb_01@163.com>
2023-08-13 20:56:29 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,67 @@
 import React, {useState, useContext} from 'react'
 
 const appContext = React.createContext(null)
+
+
 export const App = () => {
+
   const [appState, setAppState] = useState({
-    user: {name: 'frank', age: 18}
+    user: {name: 'name', age: 18}
   })
-  const contextValue = {appState, setAppState}
+
+  const contextValue = { appState, setAppState }
+  
   return (
     <appContext.Provider value={contextValue}>
-      <大儿子/>
-      <二儿子/>
-      <幺儿子/>
+      <Children1/>
+      <Children2/>
+      <Children3/>
     </appContext.Provider>
   )
-}
-const 大儿子 = () => <section>大儿子<User/></section>
-const 二儿子 = () => <section>二儿子<UserModifier/></section>
-const 幺儿子 = () => <section>幺儿子</section>
-const User = () => {
-  const contextValue = useContext(appContext)
-  return <div>User:{contextValue.appState.user.name}</div>
 
 }
-const UserModifier = () => {
-  const {appState, setAppState} = useContext(appContext)
+
+
+const createNewState = (state, actionType, actionData) => {
+  if (actionType === 'updateUser') {
+    return {
+      ...state,
+      user: {
+        ...state.user,
+        ...actionData
+      }
+    }
+  }
+}
+
+
+
+
+
+const Children1 = () => {
+  const contextValue = useContext(appContext)
+  return (
+      <section>
+        children1:
+        { contextValue.appState.user.name }
+      </section>
+    )
+}
+
+const Children2 = () => {
+  const { appState, setAppState } = useContext(appContext)
+  
   const onChange = (e) => {
     appState.user.name = e.target.value
-    setAppState({...contextValue.appState})
+    setAppState({...appState})
   }
-  return <div>
-    <input value={contextValue.appState.user.name}
-      onChange={onChange}/>
-  </div>
-}
 
+  return (
+    <section>
+      children2
+      <input value={appState.user.name} onChange={onChange}/>
+    </section>
+  )
+} 
+
+const Children3 = () => <section>children3</section>
